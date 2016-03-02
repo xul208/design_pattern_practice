3149ad866d769e2f8af18bca99c2e47a92b8f931
Eric Xu Li <lixulehigh@gmail.com>
2016-03-02 11:09:59 -0500

    first success
    the complete first example

---
+++ MallardDuck.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class MallardDuck extends Duck {
+    public MallardDuck() {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+
+    public void display() {
+        System.out.println("I'm a real Mallard duck");
+    }
+}
